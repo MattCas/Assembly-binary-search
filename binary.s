c87b7fad68eae0fdd16bd5ac72bb624bf5f38297
Matthias Casula <matt@matthiass-MacBook-Pro.local>
2016-02-19 18:46:11 +0000

    Initial commit
    Just comments containing TODOs.

---
+++ binary.s
@@ -0,0 +1,13 @@
+#read input numbers until 0 is reached
+
+#read in digits to look for in the array
+
+#perform search
+	
+	#print YES if found
+	
+	#print NO if not found
+	
+#exit
+
+#create binary search function
