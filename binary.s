1de9d7279a0ecd3088e727b9016dce0a2437e2c6
Matthias Casula <matt@matthiass-MacBook-Pro.local>
2016-03-04 01:21:26 +0000

    Search partially working
    Search only works on lower half of the list of inputs.

--- binary.s
+++ binary.s
@@ -5,8 +5,8 @@
 	.data
 	.align 2
 ### Output strings
-yes:	.asciiz "\nYES"
-no:	.asciiz "\nNO"
+yes: .asciiz "\nYES\n"
+no:	.asciiz "\nNO\n"
 listIn:	.asciiz "Enter list of numbers \n"
 numIn:	.asciiz "Enter a number to search for \n"
 ### Standard preliminaries
@@ -58,24 +58,58 @@ SAFEXT:
 # Binary search subroutine
 binarysearch:	
 	li $t3, 2; # Store 2 in $t3	
-	mul $a2, $t0, 4; # Offset in stack pointer from end of list to start(Counter*4)<-ERRORHERE
+	mul $a2, $t0, 4; # Offset in stack pointer from end of list to start(Counter*4)
 	sub $t2, $t7, $a2; # Left bound in $t2
+	# While loop condition
+	slt $t1, $t7, $t2 # Sets t1 to 1 if $t2 > $t7 (left > right)
+	beq $t1, 1, PRINTNO # if $t2 > $t7 (left >right)
+	li $t1, 0; # Set t1 = 0 before reusing it below
+	
 	sub $a3, $t7, $t2; # (Right - left) bound 
 	div $a3, $t3; # Divide (Right - left) by 2
 	mflo $v0 # Quotient to $v0 -> Is the offset to the mid value
 	move $t5, $v0; # Move offset to mid -> t5
 	add $t4, $t2, $t5; # (left+offset) => Pointer to mid value
+	lw $a1, ($t4);
+	div $a2, $t3; # Divide counter by 2
+	mflo $v0 # Quotient to $v0 -> Counter/2
+	move $t0, $v0; # Move new counter/2 back to t3 where original counter was
+	beq $t6, $a1, PRINTYES; # If testNum == midValue then print YES
+	# All the if(s) 
+	slt $t1, $t6, $a1 # Sets t1 to 1 if $t4 > $t6 (midVal > testNum)
+	
+	# Print midval (Just for testing for now)!
+		li 	$v0, 1		; # System call code 1 for print int
+		move $a0, $t1	 ; # Argument midval
+		syscall			; # Print the midval
+	
+	beq $t1, 1, BIGGER # if $t4 = $t6 (midVal < testNum)-> left = mid + 4bytes (t2)	
+	bne $t1, 1, SMALLER # if $t4 > $t6 (midVal > testNum)-> right = mid - 4bytes (t7)
 	
 	# Print midval (Just for testing for now)!
 	#	li 	$v0, 1		; # System call code 1 for print int
 	#	lw $a0, ($t4)	 ; # Argument midval
 	#	syscall			; # Print the midval
-
+	
 # Return to SEARCHVALIN where jal was called (This will be last command)	
 	jr $ra;
-	
-# Print yes (Just for testing for now)!
+
+SMALLER:
+	sub $t7, $t4, 4; # RightBound = mid-1 (4bytes)
+	j binarysearch;
+BIGGER:
+	add $t2, $t4, 4; # LeftBound = mid+1 (4bytes)
+	j binarysearch;
+PRINTYES:	
+# Print yes 
 	li 	$v0,4		; # System call code 4 for print string
 	la 	$a0,yes		; # Argument string as input
 	syscall			; # Print the string
+	j SEARCHVALIN;
+PRINTNO:	
+# Print no 
+	li 	$v0,4		; # System call code 4 for print string
+	la 	$a0,no		; # Argument string as input
+	syscall			; # Print the string
+	j SEARCHVALIN;
 
